595d1fa1c2caa57c448cf79bdbd86b7aa61db243
gkatejka <kate.golovkina@gmail.com>
2016-09-27 16:08:03 +0300

    added jQuery for search block

--- src/js/search.js
+++ src/js/search.js
@@ -1,5 +1,15 @@
-$(document).ready(function(){
-  $( ".header__info-box" ).click(function() {
-    $( this ).toggleClass( "header__search--open" );
+'use strict';
+
+$(function(){
+    
+  $('.header__search-icon--open' ).click(function(){
+    $('.header__search').addClass('header__search--open');
+    $('.header__search-icon').removeClass('header__search-icon--open');
   });
+
+  $('.header__search-close' ).click(function(){
+    $('.header__search').removeClass('header__search--open');
+    $('.header__search-icon').addClass('header__search-icon--open');
+  });
+
 });
