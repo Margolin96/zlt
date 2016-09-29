2f5c65f60f5cc03c8c11ee9c205e0201601781ce
gkatejka <kate.golovkina@gmail.com>
2016-09-29 17:30:35 +0300

    finished adaptive for main page

--- src/js/main.js
+++ src/js/main.js
@@ -8,6 +8,18 @@ $(function(){
 
   $('.menu-mobile__close' ).click(function(){
     $('.menu-mobile').removeClass('menu-mobile--open');
+    $('.menu-mobile__sub-list').removeClass('menu-mobile__sub-list--open');
+    $('.menu-mobile').removeClass('menu-mobile--submenu');
+  });
+
+  $('.menu-mobile__list-item--arrow a' ).click(function(){
+    $('.menu-mobile__sub-list').addClass('menu-mobile__sub-list--open');
+    $('.menu-mobile').addClass('menu-mobile--submenu');
+  });
+
+  $('.menu-mobile__sub-list li:first-child a' ).click(function(){
+    $('.menu-mobile__sub-list').removeClass('menu-mobile__sub-list--open');
+    $('.menu-mobile').removeClass('menu-mobile--submenu');
   });
 
   $('.main-slider').slick({
