0bd6428a623fba93b7a7586418982fbf5f77a9b1
gkatejka <kate.golovkina@gmail.com>
2016-10-02 18:30:16 +0300

    reload docs

--- docs/js/script.js
+++ docs/js/script.js
@@ -49,6 +49,22 @@ $(function(){
     infinite: true
   });
 
+  $('.product-slider__for').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: '.product-slider__nav'
+  });
+
+  $('.product-slider__nav').slick({
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    asNavFor: '.product-slider__for',
+    dots: false,
+    focusOnSelect: true
+  });
+
   $('.seo').readmore({
     collapsedHeight: 160,
     moreLink: '<button class="btn btn--more">Подробнее</button>',
@@ -199,3 +215,47 @@ $(function() {
     }
   });
 });
+'use strict';
+
+(function() {
+  var tabControls = document.querySelectorAll('.js-tab-control');
+
+  if (tabControls) {
+    tabControls = Array.prototype.slice.call(tabControls, 0);
+
+    tabControls.forEach(function(tabControl) {
+      tabControl.addEventListener('click', function(event) {
+        event.preventDefault();
+
+        var target = tabControl.dataset.target;
+        var tabParent = tabControl.parentNode.classList[0];
+
+        var tabParents = document.querySelectorAll('.' + tabParent);
+        tabParents = Array.prototype.slice.call(tabParents, 0);
+
+        tabParents.forEach(function(stab) {
+          stab.classList.remove(tabParent + '--active');
+        });
+
+        tabControl.parentNode.classList.add(tabParent + '--active');
+
+        var targetBlock = document.querySelector('.' + target);
+
+        if (targetBlock) {
+          var allTabs = document.querySelectorAll('.js-tab');
+          allTabs = Array.prototype.slice.call(allTabs, 0);
+
+          allTabs.forEach(function(tab) {
+            if (tab === targetBlock) {
+              tab.classList.add('js-tab--active');
+            } else {
+              tab.classList.remove('js-tab--active');
+            }
+          });
+        }
+
+
+      });
+    });
+  }
+})();
