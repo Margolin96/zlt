a6c3ec5ad3f1f596bb08ca58cf08d598fbcdd575
gkatejka <kate.golovkina@gmail.com>
2016-10-12 04:02:26 +0300

    added tablet and mobile version for souvenirs with filter page

--- src/js/main.js
+++ src/js/main.js
@@ -6,25 +6,30 @@ $(function(){
     $('.menu-mobile').addClass('menu-mobile--open');
   });
 
+
   $('.menu-mobile__close' ).click(function(){
     $('.menu-mobile').removeClass('menu-mobile--open');
     $('.menu-mobile__sub-list').removeClass('menu-mobile__sub-list--open');
     $('.menu-mobile').removeClass('menu-mobile--submenu');
   });
 
+
   $('.menu-mobile__list-item--arrow a' ).click(function(){
     $('.menu-mobile__sub-list').addClass('menu-mobile__sub-list--open');
     $('.menu-mobile').addClass('menu-mobile--submenu');
   });
+
 
   $('.menu-mobile__sub-list li:first-child a' ).click(function(){
     $('.menu-mobile__sub-list').removeClass('menu-mobile__sub-list--open');
     $('.menu-mobile').removeClass('menu-mobile--submenu');
   });
 
+
   $('.main-slider').slick({
     infinite: true
   });
+
 
   $('.product-slider__for').slick({
     slidesToShow: 1,
@@ -34,6 +39,7 @@ $(function(){
     asNavFor: '.product-slider__nav'
   });
 
+
   $('.product-slider__nav').slick({
     slidesToShow: 6,
     slidesToScroll: 1,
@@ -42,13 +48,21 @@ $(function(){
     focusOnSelect: true
   });
 
+
   $('.filter__price' ).click(function(){
     $('.filter__price').toggleClass('filter__price--active');
   });
 
+
   $('.category-nav__group-name' ).click(function(){
     $(this).toggleClass('category-nav__group-name--open');
   });
+
+  $('#open-catalog' ).click(function(){
+    $('.catalog__left').addClass('catalog__left--open');
+    $('.catalog__right').addClass('catalog__right--close');
+  });
+
 
   $('.seo').readmore({
     collapsedHeight: 160,
