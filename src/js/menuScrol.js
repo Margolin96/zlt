a0547ed5471e49cf2cf2d7fad310c5a1b4aa4266
gkatejka <kate.golovkina@gmail.com>
2016-10-16 22:33:00 +0300

    added modal 'ask question'

--- src/js/menuScrol.js
+++ src/js/menuScrol.js
@@ -1,6 +1,6 @@
 $(window).on('scroll', function (){
   
-  if ($(this).scrollTop() > 700){
+  if ($(this).scrollTop() > 500){
       $('.header').addClass('header--fixed');
     } else {
       $('.header').removeClass('header--fixed');
