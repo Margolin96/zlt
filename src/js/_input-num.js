e53f6abf862479da4797703bb4661f89d44dea66
gkatejka <kate.golovkina@gmail.com>
2016-10-17 13:06:23 +0300

    added scripts

---
+++ src/js/_input-num.js
@@ -0,0 +1,59 @@
+'use strict';
+
+$(function($){
+  $.fn.inputNum = function() {
+
+    var input = $(this.get());
+
+    var val = input.val() === '' ? 0 : input.val();
+    var min = input.attr('min') === undefined ? 0 : input.attr('min');
+    var max = input.attr('max') === undefined ? 0 : input.attr('max');
+
+    input.replaceWith('<div class="counter" data-min="'+min+'" data-max="'+max+'">\
+      <span class="counter__nav counter__nav--minus">-</span>\
+      <input class="counter__number counter__field" type="number" value="'+val+'">\
+      <span class="counter__nav counter__nav--plus">+</span>\
+    </div>');
+
+    function inputNumUpdate(nav, change) {
+      var counter = $(nav).closest('.counter');
+      var field = counter.find('.counter__field');
+      var number = counter.find('.counter__number');
+      var value = field.val() * 1;
+
+      value += change * 1;
+
+      if (change > 0) {
+        if ((counter.attr('data-max') != 0) && (value > counter.attr('data-max'))) {
+          value = counter.attr('data-max');
+        }
+      } else {
+        if (value < counter.attr('data-min')) {
+          value = counter.attr('data-min');
+        }
+      }
+
+      field.val(value);
+      number.text(value);
+      field.trigger('input-num-change');
+    };
+
+    $('.counter').on('click', '.counter__nav--minus', function(e) {
+      e.preventDefault();
+
+      inputNumUpdate(this, -1);
+      $(this).find('.counter__field').trigger('input-num-minus');
+    });
+
+    $('.counter').on('click', '.counter__nav--plus', function(e) {
+      e.preventDefault();
+
+      inputNumUpdate(this, 1);
+      $(this).find('.counter__field').trigger('input-num-plus');
+    });
+
+    $('.counter').on('change', '.counter__field', function(e) {
+      $(this).trigger('input-num-change');
+    });
+  };
+});
--- src/js/_repeater.js
+++ src/js/_repeater.js
@@ -1,112 +1,112 @@
 'use strict';
 
 $(function($) {
-	$.fn.repeater = function(atts_custom) {
-		var atts_default = {
-			container: '.form__row__group',
-			row: '.form__row',
-			add: '.btn--add',
-			remove: '.btn--remove',
-			template: '.form__row--template',
-			max: 0
-		}
+  $.fn.repeater = function(atts_custom) {
+    var atts_default = {
+      container: '.form__row__group',
+      row: '.form__row',
+      add: '.btn--add',
+      remove: '.btn--remove',
+      template: '.form__row--template',
+      max: 0
+    };
 
-		if ($(this.selector).length == 0) return false
+    if ($(this.selector).length == 0) return false;
 
-		var atts = $.extend({}, atts_default, atts_custom);
+    var atts = $.extend({}, atts_default, atts_custom);
 
-		initialize(this);
-		row_add(this);
+    initialize(this);
+    row_add(this);
 
-		function initialize(parent) {
-			parent = parent === undefined ? atts.container : parent;
+    function initialize(parent) {
+      parent = parent === undefined ? atts.container : parent;
 
-			$(parent).each(function(index, element) {
-				var container = this;
+      $(parent).each(function(index, element) {
+        var container = this;
 
-				$(container).children(atts.template).hide().find(':input').each(function() {
-					$(this).prop('disabled', true);
-				});
+        $(container).children(atts.template).hide().find(':input').each(function() {
+          $(this).prop('disabled', true);
+        });
 
-				var row_count = $(container).children(atts.row).filter(function() {
-					return !$(this).hasClass(atts.template.replace('.', ''));
-				}).length;
+        var row_count = $(container).children(atts.row).filter(function() {
+          return !$(this).hasClass(atts.template.replace('.', ''));
+        }).length;
 
-				$(container).attr('data-row-count', row_count);
+        $(container).attr('data-row-count', row_count);
 
-				$(container).on('click', atts.add, function(event) {
-					event.stopImmediatePropagation();
+        $(container).on('click', atts.add, function(event) {
+          event.stopImmediatePropagation();
 
-					row_add(container);
-				});
+          row_add(container);
+        });
 
-				$(container).on('click', atts.remove, function(event) {
-					event.stopImmediatePropagation();
+        $(container).on('click', atts.remove, function(event) {
+          event.stopImmediatePropagation();
 
-					row_remove(this, container);
-				});
-			});
-		}
+          row_remove(this, container);
+        });
+      });
+    }
 
-		function row_add(container) {
-			var row_template = $($(container).children(atts.template).clone().removeClass(atts.template.replace('.', ''))[0].outerHTML);
-			var row_count = $(container).attr('data-row-count');
-			var row_first = $(container).find(atts.row+':not('+atts.template+')')[0];
+    function row_add(container) {
+      var row_template = $($(container).children(atts.template).clone().removeClass(atts.template.replace('.', ''))[0].outerHTML);
+      var row_count = $(container).attr('data-row-count');
+      var row_first = $(container).find(atts.row+':not('+atts.template+')')[0];
 
-			$(row_template).find(':input').each(function() {
-				$(this).prop('disabled', false);
-			});
+      $(row_template).find(':input').each(function() {
+        $(this).prop('disabled', false);
+      });
 
-			if (atts.max > 0) {
-				if (row_count >= atts.max - 1) {
-					$(row_first).find(atts.add).hide()
-					$(row_first).find(atts.remove).show()
-				}
-				if (row_count >= atts.max) return false
-			}
+      if (atts.max > 0) {
+        if (row_count >= atts.max - 1) {
+          $(row_first).find(atts.add).hide();
+          $(row_first).find(atts.remove).show();
+        }
+        if (row_count >= atts.max) return false;
+      }
 
-			var new_row = $(row_template).show().appendTo(container);
+      var new_row = $(row_template).show().appendTo(container);
 
-			if (row_count > 0) { new_row.find(atts.add).hide() }
-			else { new_row.find(atts.remove).hide() }
+      if (row_count > 0) { new_row.find(atts.add).hide() }
+      else { new_row.find(atts.remove).hide() }
 
-			$(container).attr('data-row-count', ++row_count);
+      $(container).attr('data-row-count', ++row_count);
 
-			after_add(container, new_row);
+      after_add(container, new_row);
 
-			initialize(container);
-		}
+      initialize(container);
+    }
 
-		function row_remove(element, container) {
-			var row = $(element).parents(atts.row).first();
-			var row_count = $(container).attr('data-row-count');
+    function row_remove(element, container) {
+      var row = $(element).parents(atts.row).first();
+      var row_count = $(container).attr('data-row-count');
 
-			if (row_count <= 1) return false;
+      if (row_count <= 1) return false;
 
-			row.remove();
-			$(container).attr('data-row-count', --row_count);
+      row.remove();
+      $(container).attr('data-row-count', --row_count);
 
-			var row_first = $(container).find(atts.row+':not('+atts.template+')')[0];
-			if (atts.max > 0) {
-				if (row_count <= atts.max) {
-					$(row_first).find(atts.add).show()
-					$(row_first).find(atts.remove).hide()
-				}
-			}
-		}
+      var row_first = $(container).find(atts.row+':not('+atts.template+')')[0];
+      if (atts.max > 0) {
+        if (row_count <= atts.max) {
+          $(row_first).find(atts.add).show();
+          $(row_first).find(atts.remove).hide();
+        }
+      }
+    }
 
-		function after_add(container, new_row) {
-			var row_count = $(container).attr('data-row-count');
+    function after_add(container, new_row) {
+      var row_count = $(container).attr('data-row-count');
 
-			row_count++;
+      row_count++;
 
-			$('*', new_row).each(function() {
-				$.each(this.attributes, function(index, element) {
-					this.value = this.value.replace(atts.row_count_placeholder, row_count - 1);
-				});
-			});
+      $('*', new_row).each(function() {
+        $.each(this.attributes, function(index, element) {
+          this.value = this.value.replace(atts.row_count_placeholder, row_count - 1);
+        });
+      });
 
-			$(container).attr('data-row-count', row_count);
-		}
-	}
+      $(container).attr('data-row-count', row_count);
+    }
+  }
 });
