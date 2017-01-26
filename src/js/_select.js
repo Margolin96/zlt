dd2447575c75fb4a073ad87ecc19c9c2b95a31f7
Margolin96 <margolin96@gmail.com>
2017-01-26 12:19:46 +0300

    Fix default selected

--- src/js/_select.js
+++ src/js/_select.js
@@ -4,20 +4,22 @@ $(function($){
   $.fn.dropdownSelect = function() {
     var select = $(this.get());
     $.each(select, function(sk, sv) {
-      sv = $(sv)
+      sv = $(sv);
 
-      var input = $('<input class="ds-field" type="hidden" name="' + sv.attr('name') + '">')
-      var ds = $('<div class="ds" data-name="' + sv.attr('name') + '">\
-        <div class="ds-top"><i class="ds-arrow"></i><div class="ds-value"></div></div>\
-        <div class="ds-list-arrow"></div><div class="ds-list"></div>\
-      </div>');
+      var input = $('<input class="ds-field" type="hidden" name="' + sv.attr('name') + '">');
+      var ds = $(`<div class="ds" data-name="` + sv.attr('name') + `">
+        <div class="ds-top"><i class="ds-arrow"></i><div class="ds-value"></div></div>
+        <div class="ds-list-arrow"></div><div class="ds-list"></div>
+      </div>`);
       var items = [];
-      ds.find('.ds-top').width(sv.width())
+      ds.find('.ds-top').width(sv.width());
 
+      var selected = -1;
+      
       $.each(sv.find('option'), function(ok, ov) {
         var value = $(ov).attr('value') === undefined ? "" : $(ov).attr('value');
-        var selected = $(ov).is(':selected') ? "selected" : "";
-        value = $('<div class="ds-option '+ selected +'" data-value="' + value + '">' + $(ov).html() + '</div>')
+        if ($(ov).is(':selected')) selected = ok;
+        value = $('<div class="ds-option" data-value="' + value + '">' + $(ov).html() + '</div>');
         items.push(value);
       });
 
@@ -25,6 +27,15 @@ $(function($){
       ds.find('.ds-value').append(items[0].html());
       ds.find('.ds-list').append(items);
       sv.replaceWith(ds);
+      
+      if (selected >= 0) {
+        var si = ds.find('.ds-option')[selected];
+        if (si) {
+          ds.find('.ds-value').html(si.html());
+          ds.find('.ds-field').val(si.attr('data-value');
+          si.addClass('selected');
+        }
+      }
 
       ds.on('click', '.ds-top', function(e) {
         e.preventDefault();
@@ -32,11 +43,11 @@ $(function($){
 
         if ($(this).closest('.ds').hasClass('open')) {
           $(this).closest('.ds').removeClass('open');
-          $(this).closest('.ds').trigger('selectClose')
+          $(this).closest('.ds').trigger('selectClose');
         } else {
           $('.ds').removeClass('open')
           $(this).closest('.ds').addClass('open');
-          $(this).closest('.ds').trigger('selectOpen')
+          $(this).closest('.ds').trigger('selectOpen');
         }
       })
 
