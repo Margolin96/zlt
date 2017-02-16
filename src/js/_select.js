b2e49b5151561de5c22989a967aa92b00493a538
Margolin96 <margolin96@gmail.com>
2017-02-16 22:36:28 +0300

    Rewrite plugin and add multiple values support

--- src/js/_select.js
+++ src/js/_select.js
@@ -2,78 +2,128 @@
 
 $(function($){
   $.fn.dropdownSelect = function() {
-    var select = $(this.get());
-    $.each(select, function(sk, sv) {
-      sv = $(sv);
+    this.each(function(sk, sv) {
+      var self = $(this)
 
-      var input = $('<input class="ds-field" type="hidden" name="' + sv.attr('name') + '">');
+      var _ds = $('<div class="ds"></div>')
+      var _ds_top = $('<div class="ds-top"><i class="ds-arrow"></i></div>')
+      var _ds_value = $('<div class="ds-value"></div>')
+      var _ds_list_arrow = $('<div class="ds-list-arrow"></div>')
+      var _ds_list = $('<div class="ds-list"></div></div>')
+      var _ds_field = self.clone().addClass('ds-field')
 
-      var ds = $('<div class="ds" data-name="' + sv.attr('name') + '">\
-        <div class="ds-top"><i class="ds-arrow"></i><div class="ds-value"></div></div>\
-        <div class="ds-list-arrow"></div><div class="ds-list"></div>\
-      </div>');
-      var items = [];
-      ds.find('.ds-top').width(sv.width());
+      _ds_field.find('option:not([value])').each(function(k, v) {
+        $(this).attr('value', this.value)
+      })
 
-      var selected = -1;
-      
-      $.each(sv.find('option'), function(ok, ov) {
-        var value = $(ov).attr('value') === undefined ? "" : $(ov).attr('value');
-        if ($(ov).is(':selected')) selected = ok;
-        value = $('<div class="ds-option" data-value="' + value + '">' + $(ov).html() + '</div>');
-        items.push(value);
-      });
+      var items = getItems(self)
+      var value = getValue(self)
 
-      ds.append(input);
-      ds.find('.ds-value').append(items[0].html());
-      ds.find('.ds-list').append(items);
-      sv.replaceWith(ds);
-      
-      if (selected >= 0) {
-        var si = $(ds.find('.ds-option')[selected]);
-        if (si.length > 0) {
-          ds.find('.ds-value').html(si.html());
-          ds.find('.ds-field').val(si.attr('data-value'));
-          si.addClass('selected');
-        }
-      }
+      _ds_value.text(value.label)
+      _ds_field.val(value.value)
 
-      ds.on('click', '.ds-top', function(e) {
-        e.preventDefault();
-        e.stopPropagation();
+      _ds_top.append(_ds_value)
+      _ds_list.append(items._items)
 
-        if ($(this).closest('.ds').hasClass('open')) {
-          $(this).closest('.ds').removeClass('open');
-          $(this).closest('.ds').trigger('selectClose');
-        } else {
-          $('.ds').removeClass('open');
-          $(this).closest('.ds').addClass('open');
-          $(this).closest('.ds').trigger('selectOpen');
-        }
-      });
+      _ds
+        .append(_ds_top)
+        .append(_ds_list_arrow)
+        .append(_ds_list)
+        .prepend(_ds_field)
 
-      ds.on('click', '.ds-option', function(e) {
-        e.preventDefault();
-        e.stopPropagation();
+      _ds_field.find('option[selected]').each(function() {
+        this.selected = true
+      })
 
-        var ds = $(this).closest('.ds');
-        var value = ds.find('.ds-value');
-        var input = ds.find('.ds-field');
+      setListeners(_ds)
 
-        input.val($(this).attr('data-value'));
-        value.html($(this).html());
-
-        ds.find('.ds-option.selected').removeClass('selected');
-        $(this).addClass('selected');
-
-        ds.removeClass('open');
-
-        ds.trigger('selectChoose');
-      });
-      
-      $(document).on('click', function(e) {
-        ds.removeClass('open');
-      });
+      self.replaceWith(_ds)
     })
-  };
-});
+
+    function getValue(select) {
+      var value = []
+      var label = []
+      select.find('option:selected').each(function(k, v) {
+        value.push(v.value)
+        label.push(v.text)
+      })
+      return {
+        value: value.join(),
+        label: label.join(', ')
+      }
+    }
+
+    function getItems(select) {
+      var items = [] // array
+      var _items = [] // html
+      var selected = []
+
+      select.find('option').each(function(k, v) {
+        v = $(v)
+
+        var item = {
+          key: v.val(),
+          value: v.text(),
+          selected: v.is(':selected')
+        }
+
+        items.push(item)
+
+        _items.push('<div'
+            +' class="ds-option '+(item.selected ? 'selected' : '')+'"'
+            +' data-selected="'+(item.selected ? 'true' : 'false')+'"'
+            +' data-value="'+(item.key)+'">'
+            +''+(item.value)
+          +'</div>')
+
+        if (item.selected) { selected.push(item) }
+      })
+
+      return {
+        items: items,
+        _items: _items.join(''),
+        selected: selected
+      }
+    }
+
+    function setListeners(_ds) {
+      _ds.on('click', '.ds-top', function(e) {
+        e.preventDefault()
+        e.stopPropagation()
+
+        var ds = $(this).closest('.ds')
+        $('.ds').not(ds).removeClass('open')
+        ds.toggleClass('open')
+      })
+
+      _ds.on('click', '.ds-option', function(e) {
+        e.preventDefault()
+        e.stopPropagation()
+
+        var ds = $(this).closest('.ds')
+        var select = ds.find('.ds-field')
+        var multiple = !!select.attr('multiple')
+        var value = $(this).attr('data-value')
+        var item = select.find('option[value="'+value+'"]')
+
+        if (item.is(':selected')) {
+          item.prop('selected', false)
+          $(this).removeClass('selected')
+        } else {
+          item.prop('selected', true)
+          $(this).addClass('selected')
+        }
+
+        if (multiple != true) {
+          select.find('option:selected:not([value="'+value+'"])').prop('selected', false)
+          ds.find('.ds-option.selected:not([data-value="'+value+'"])').removeClass('selected')
+          ds.removeClass('open')
+        }
+
+        value = getValue(select)
+
+        ds.find('.ds-value').text(value.label)
+      })
+    }
+  }
+})
